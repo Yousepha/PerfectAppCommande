835ff5d3a0474a9873aa587698d49f7933e8e102
Yousepha <yousane99@gmail.com>
2025-04-16 15:17:41 +0000

    middlewareUpdate

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,22 @@
-export { auth as middleware } from "@/auth"
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
+
+  if (!token && request.nextUrl.pathname.startsWith("/admin")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"], // ajuste selon les routes que tu veux protéger
+};
+
+
+// export { auth as middleware } from "@/auth"
